beddc8ac35c8cdfe91dd6399d131a86c9623295e
Laurish <daniel.skshipek@hotmail.com>
2026-04-01 10:50:55 +0200

    fix: update base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/sandra-portfolio/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
