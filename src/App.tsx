7e4834a464870b8d17b4c1221cce0d9b8c27a3bc
Laurish <daniel.skshipek@hotmail.com>
2026-04-01 11:49:50 +0200

    fix: remove basename from BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/sandra-portfolio/">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
